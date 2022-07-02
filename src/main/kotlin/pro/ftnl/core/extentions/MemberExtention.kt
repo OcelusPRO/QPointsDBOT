38e596556396516a356a587c1f1803fe3130ce4e
OcelusPRO <ocelus1999@hotmail.com>
2022-07-02 19:03:20 +0200

    update methods typo

--- src/main/kotlin/pro/ftnl/core/extentions/MemberExtention.kt
+++ src/main/kotlin/pro/ftnl/core/extentions/MemberExtention.kt
@@ -3,7 +3,7 @@ package pro.ftnl.core.extentions
 import net.dv8tion.jda.api.entities.Member
 import pro.ftnl.core.QPointAPI
 
-fun Member.addQpoint(qpoint: Int) = QPointAPI().addQpoints(qpoint, listOf(this.id))
-fun Member.removeQpoint(qpoint: Int) = QPointAPI().removeQpoints(qpoint, listOf(this.id))
-fun Member.setQpoint(qpoint: Int) = QPointAPI().setQpoints(qpoint, listOf(this.id))
+fun Member.addQPoint(qPoints: Int) = QPointAPI().addQpoints(qPoints, listOf(this.id))
+fun Member.removeQPoint(qPoints: Int) = QPointAPI().removeQpoints(qPoints, listOf(this.id))
+fun Member.setQPoint(qPoints: Int) = QPointAPI().setQpoints(qPoints, listOf(this.id))
 fun Member.getMemberData() = QPointAPI().getMemberData(this.id)
